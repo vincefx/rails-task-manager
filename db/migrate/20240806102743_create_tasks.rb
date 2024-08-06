6cd27a404d8b65f5370b89f83005683514551b4b
vincent gannier <vincent.gannier@gmail.com>
2024-08-06 12:28:27 +0200

    rails new

--- db/migrate/20240806102743_create_tasks.rb
+++ db/migrate/20240806102743_create_tasks.rb
@@ -1,6 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed
 
       t.timestamps
     end
